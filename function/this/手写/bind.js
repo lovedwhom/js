83aea83e8e49f363d2fbd8427112e6a6479a25b6
hebin <hebin>
2020-07-09 09:51:21 +0800

    手写 apply call bind

--- function/this/手写/bind.js
+++ function/this/手写/bind.js
@@ -8,6 +8,8 @@ Function.prototype.myBind = function (context) {
     fn = this;
     return function Fn() {
         return fn.apply(
+            //考虑用new调用的情况
+            //还有一个特点就是bind调用的时候可以传参,调用之后生成的新的函数也可以传参
             this instanceof Fn ? this :context,
             args.concat(...arguments)
         )
